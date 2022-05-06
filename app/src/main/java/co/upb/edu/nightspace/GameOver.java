d8eaacadceed719cb7396a339377561698e696d8
Candre <carlos.uribecupb.edu.co>
2022-05-06 08:48:19 -0500

    Puntaje maximo implementado

--- app/src/main/java/co/upb/edu/nightspace/GameOver.java
+++ app/src/main/java/co/upb/edu/nightspace/GameOver.java
@@ -1,9 +1,12 @@
 package co.upb.edu.nightspace;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,22 @@ public class GameOver extends AppCompatActivity {
         tvPoints = findViewById(R.id.tvPoints);
         tvPoints.setText("" + points);
 
+        Context context = this;
+        SharedPreferences sharedprefs = getSharedPreferences("puntajeMaximo", context.MODE_PRIVATE);
+        String valor = sharedprefs.getString("Max", "Vacio");
+        if(valor.equals("Vacio")){
+            SharedPreferences.Editor editor = sharedprefs.edit();
+            editor.putString("Max", String.valueOf(points));
+            editor.commit();
+        }
+        else if(Integer.parseInt(valor)<points){
+            SharedPreferences.Editor editor = sharedprefs.edit();
+            editor.putString("Max", String.valueOf(points));
+            editor.commit();
+        }
+        valor = sharedprefs.getString("Max", "Vacio");
+        TextView maximo = (TextView) findViewById(R.id.tvMax);
+        maximo.setText(valor);
     }
 
 
